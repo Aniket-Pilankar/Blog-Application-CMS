3e4c87c9aae089f8746ab77debac3e8843a69485
Aniket Pilankar <aniketpilankar1998@gmail.com>
2024-03-30 02:44:56 +0530

    (feat): add blog detail page content

--- components/shared/BlogDetail.jsx
+++ components/shared/BlogDetail.jsx
@@ -1,4 +1,6 @@
-import { Card } from "flowbite-react";
+import { getHTMLContent } from "@/app/_utils/helper";
+import { Avatar, Card } from "flowbite-react";
+import moment from "moment";
 import Image from "next/image";
 import React from "react";
 
@@ -7,23 +9,36 @@ const BlogDetail = ({ blog }) => {
   return (
     <div>
       <Card
-        className="max-w-sm"
+        className="max-w-3xl"
         renderImage={() => (
           <Image
-            width={500}
+            width={800}
             height={500}
             src={blog.displayImage.url}
-            alt="image 1"
+            alt="poster"
           />
         )}
       >
-        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          Noteworthy technology acquisitions 2021
-        </h5>
-        <p className="font-normal text-gray-700 dark:text-gray-400">
-          Here are the biggest enterprise technology acquisitions of 2021 so
-          far, in reverse chronological order.
-        </p>
+        <div className="hidden md:flex items-center justify-between lg:mb-0 lg:w-auto mr-8 items-center gap-5">
+          <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {blog.title}
+          </h5>
+          <Avatar img={blog.author.photo.url} rounded>
+            <div className="space-y-1 font-medium dark:text-white">
+              <div>{blog.author.name}</div>
+              <div className="text-sm text-gray-500 dark:text-gray-400">
+                {moment(blog.createdAt).format("MMM DD, YYYY")}
+              </div>
+            </div>
+          </Avatar>
+        </div>
+        {blog.content.raw.children.map((objType, index) => {
+          const children = objType.children.map((item, itemindex) =>
+            getHTMLContent(itemindex, item.text, item)
+          );
+
+          return getHTMLContent(index, children, objType, objType.type);
+        })}
       </Card>
     </div>
   );
